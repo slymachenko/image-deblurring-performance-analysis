e0815fdd4f802592f354b34db2b49e8579179e6e
Arsenii <senya.s408@gmail.com>
2025-05-07 21:28:05 +0200

    Updated INIT_IMAGES_DATASET_PATH to match official HQ-50K huggingface repository

--- utils/constants.py
+++ utils/constants.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).resolve().parent.parent
 
-INIT_IMAGES_DATASET_PATH = ROOT_PATH / "data/HQ-50k/test"
+INIT_IMAGES_DATASET_PATH = ROOT_PATH / "data/HQ-50K/test"
 DIR_DATASET_PATH = ROOT_PATH / "data/image-deblurring-performance-analysis"
 
 MAIN_DATASET_PATH = DIR_DATASET_PATH / "image_deblurring_dataset.parquet"
