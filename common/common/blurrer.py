b6f81e786f5a44c52afe03add0af7c0bdcaef1e8
Arsenii <senya.s408@gmail.com>
2025-05-16 22:46:29 +0200

    fix: type convertion for ints

--- common/common/blurrer.py
+++ common/common/blurrer.py
@@ -3,12 +3,14 @@ import cv2
 
 def gen_gaussian_kernel(size, sigma):
     """Generate a Gaussian blur kernel."""
-    k1d = cv2.getGaussianKernel(size, sigma)
+    k1d = cv2.getGaussianKernel(int(size), sigma)
     kernel = k1d @ k1d.T
     return kernel
 
 def gen_motion_kernel(length, angle):
     """Generate a motion blur kernel based on length and angle."""
+    if not isinstance(length, int):
+        length = int(length)
     if length % 2 == 0:
         length += 1
 
@@ -28,4 +30,7 @@ def gen_motion_kernel(length, angle):
 
 def gen_box_kernel(size):
     """Generate a box blur kernel."""
+    if not isinstance(size, int):
+        size = int(size)
+
     return np.ones((size, size), dtype=np.float32) / (size * size)
